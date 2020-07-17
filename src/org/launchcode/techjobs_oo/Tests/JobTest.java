1f76f3ddf2f7a48b5ee8e673a783b6bd0ba93c47
webby12333 <webby12333@yahoo.com>
2020-07-16 20:56:20 -0500

    Step 4.2

---
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -0,0 +1,12 @@
+package org.launchcode.techjobs_oo.Tests;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class JobTest {
+
+    @Test
+    public void testSettingJobId() {
+
+    }
+}
